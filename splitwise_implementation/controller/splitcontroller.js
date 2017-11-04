b32909fabe96582d4499262eca90ce6f10dfb750
sunchitsharma <sunchitsharma8@gmail.com>
2017-11-04 23:21:04 +0530

    Data Base connected to register page

--- splitwise_implementation/controller/splitcontroller.js
+++ splitwise_implementation/controller/splitcontroller.js
@@ -5,6 +5,9 @@
 
 //DATABASE/////////////////////////////////////////////////////////////////////////////////////////////////////////
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 
 mongoose.connect('mongodb://localhost/mydb',{useMongoClient: true});
 
@@ -35,21 +38,24 @@ app.get('/login',function(req,res){
   res.render('login');
 });
 // For rendering the regiter page
+
 app.get('/register',function(req,res){
+  res.render('register');
+});
+
+app.post('/register_db',urlencodedParser ,function(req,res){
 
 
    const user = new RegisterUser({
-     username:"satish",
-     password:"iamcool",
-     email:"satish@gmail.com"
+     username:req.body.user,
+     password:req.body.pass,
+     email:req.body.email_id
    });
 
    user.save().then(function(){
      console.log("Inserted in the DB from regiter page");
    })
 
-
-  res.render('register');
 });
 
 };
